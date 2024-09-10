acbfc0fbef4db75eba6bf115120bbd4ee76f0dd6
ducngo <duc.ngo@salto.vn>
2024-09-10 15:00:27 +0700

    feat: etst

--- config/database.js
+++ config/database.js
@@ -1,4 +1,14 @@
 import mongoose from "mongoose";
+import { MongoClient } from "mongodb";
+
+const test = async() => {
+  const client1 = new MongoClient(process.env.MONGODB_URI);
+  const clientPromise = client1.connect();
+
+  const client = await clientPromise;
+  const db = client.db(process.env.DB_NAME);
+  return { db, client };
+}
 
 const connectDB = async () => {
   if(mongoose.connections[0].readyState){
@@ -8,6 +18,8 @@ const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Mongodb connected')
+    await test();
+    console.log("TEST PASSED");
     return true;
   } catch (error) {
     console.log(error)
